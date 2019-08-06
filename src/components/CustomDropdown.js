b9f870f3c7fd39152bfae1dbb8d6ac59fa751959
rajeshkumarramaswamy <rajeshkramaswamy@gmail.com>
2019-08-06 15:55:45 +0530

    Added text area with rows

--- src/components/CustomDropdown.js
+++ src/components/CustomDropdown.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Dropdown, Button, Grid } from "semantic-ui-react";
+import { Dropdown, Button, Grid, TextArea, Input } from "semantic-ui-react";
 import _ from 'lodash';
 import YAML from 'json-to-pretty-yaml';
-import ReactJson from 'react-json-view'
 import {yamprint} from 'yamprint';
+import yaml from 'yamljs';
 
 
 class CustomDropdown extends Component {
@@ -13,31 +13,45 @@ class CustomDropdown extends Component {
             dp1: false,
             dp2: false,
             dp3: false,
-            dataObject: []
+            mypackage:'',
+            description:'',
+            dataObject: [],
+            sampleData: [
+                {
+                  "name": "Download the nginx package but do not install it",
+                  "yum": {
+                    "name": [
+                      "nginx",
+                      "postgresql",
+                      "postgresql-server"
+                    ],
+                    "state": "latest",
+                    "download_only": true
+                  }
+                },
+                {
+                  "name": "Update the repository cache and update package \"nginx\" to latest version using default release squeeze-backport",
+                  "apt": {
+                    "name": "nginx",
+                    "state": "latest"
+                  }
+                }
+              ],
+            // yamlText: ''
         }
     }
 
     handleChangefordp1 = (e, { value }) => this.setState({ dp1: value })
     handleChangefordp2 = (e, { value }) => this.setState({ dp2: value })
     handleChangefordp3 = (e, { value }) => this.setState({ dp3: value })
-
-    onHandleSubmit = () => {
-        let sample1 = {
-            [this.state.dp1]: {
-                'name': 'example1',
-                [this.state.dp2]: this.state.dp3
-            }
-        }
-        console.log('sampe', sample1);
-        // this.setState({dataObject: samdb})
-    }
+    handleTextChange = (e) => this.setState({[e.target.name]: e.target.value})
 
     onAddItem = () => {
         // not allowed AND not working
         let sample1 = {
-            'name': 'Enter your description here',
+            'name': this.state.description,
             [this.state.dp1]: {
-                'name': ['package','package2'],
+                'name': this.state.mypackage.split(','),
                 [this.state.dp2]: this.state.dp3
             }
         }
@@ -45,22 +59,39 @@ class CustomDropdown extends Component {
             const list = state.dataObject.push(sample1);
             return list
         });
+        // const list = this.state.dataObject.push(sample1)
+
+        // this.setState({  yamlText: YAML.stringify(this.state.dataObject.push(sample1)) })
+
         
     };
 
+    textAreaChange = (e) => {
+        e.preventDefault()
+        this.setState({ dataObject: YAML.stringify((this.state.dataObject)) })
+    }
+
+    refreshTextArea = () => {
+        this.setState({dataObject: []})
+    }
+
     render() {
         const { data } = this.props
-        const { dp1, dp2, dp3, dataObject } = this.state
+        const { dp1, dp2, dp3, dataObject, description, mypackage, sampleData } = this.state
         let dropdown1 = _.map(_.uniq(_.map(data, x => x['Module'])), (val) => ({ 'key': val, 'value': val, 'text': val }))
         let dropdown2 = _.map(_.uniq(_.map(data, x => x['Parameters'])), (val) => ({ 'key': val, 'value': val, 'text': val }))
         let dropdown3 = _.map(_.uniq(_.map(data, x => x['Choices'])), (val) => ({ 'key': val, 'value': val, 'text': val }))
         let yamlText = dataObject ? YAML.stringify(dataObject) : null
-        let yamlTextA = dataObject ? yamprint(dataObject) : null
-        console.log('yamlText', dataObject);
+        // let yamlText = dataObject ? YAML.stringify(sampleData) : null
+        // let yamlTextA = dataObject ? yamprint(dataObject) : null
+        console.log('yamlText', dataObject, sampleData);
         return (
             <Grid.Row columns={2} style={{marginLeft: '100px'}}>
                 {!_.isEmpty(data) ?
                     <Grid  divider columns={2} width={6}>
+                        <Grid.Row>
+                        <Input value={description} onChange={this.handleTextChange} name='description' />
+                        </Grid.Row>
                         <Grid.Row textAlign='center'>
                             <Dropdown
                                 placeholder="Module"
@@ -68,6 +99,7 @@ class CustomDropdown extends Component {
                                 options={dropdown1}
                                 onChange={this.handleChangefordp1}
                             />
+                            <Input value={mypackage} onChange={this.handleTextChange} name='mypackage' />
 
                         </Grid.Row>
                         <Grid.Row textAlign='center'>
@@ -89,7 +121,8 @@ class CustomDropdown extends Component {
 
                         <Grid.Row textAlign='center'>
                             <Button primary disabled={!(dp1 && dp2 && dp3)} onClick={this.onAddItem}>Submit</Button>
-                            <Button primary onClick={this.props.refresh}>Refresh</Button>
+                            <Button primary onClick={this.refreshTextArea}>Refresh</Button>
+                            {/* <Button primary onClick={this.exportData}>Export</Button> */}
                         </Grid.Row>
                     </Grid> : null
                 }
@@ -98,9 +131,9 @@ class CustomDropdown extends Component {
 
                         // <ReactJson src={dataObject} />
                         <div>
-                            <pre>
-                            {yamlText}
-                            </pre>
+                            {/* <TextArea as='textarea' rows='20' onChange={this.textAreaChange} value={yamlText}>{yamlText}</TextArea> */}
+                            <textarea rows='20' cols='50' onChange={this.textAreaChange} value={yamlText} disabled>{yamlText}</textarea>
+                            
                         </div>
                          : null
                     }
--- src/components/ExcelReader.js
+++ src/components/ExcelReader.js
@@ -55,11 +55,11 @@ class ExcelReader extends Component {
       const data = XLSX.utils.sheet_to_json(ws);
       /* Update state */
       if (!_.isEmpty(_.takeWhile(data, 'Module'))) {
-        this.setState({ data: data, hideForm: true,cols: make_cols(ws['!ref']) }, () => {
+        this.setState({ data: data, hideForm: true, cols: make_cols(ws['!ref']) }, () => {
           // console.log(JSON.stringify(this.state.data, null, 2));
         });
       } else {
-        this.setState({showError: true, disableButton: true})
+        this.setState({ showError: true, disableButton: true })
         return false;
       }
 
@@ -73,65 +73,64 @@ class ExcelReader extends Component {
   }
 
   handleRefresh = () => {
-    this.setState({hideForm: false, data: [], disableButton: true})
+    this.setState({ hideForm: false, data: [], disableButton: true })
   }
 
   handleClose = () => this.setState({ showError: false, disableButton: true })
 
   render() {
-    console.log('showfrm', _.isEmpty(this.state.data), this.state.showForm);
     return (
       <>
-      <Tab.Pane attached={false} className="center-div" style={{display: !_.isEmpty(this.state.data) && this.state.hideForm ? 'None' : 'block'}}>
-            <Message>Upload files to convert Excel data to YAML or JSON.</Message>
-            <Form onSubmit={this.onFormSubmit}>
-              <Form.Field>
-                <label>File input & upload </label>
-                <Button as="label" htmlFor="file" type="button" animated="fade">
-                  <Button.Content visible>
-                    <Icon name="file" />
-                  </Button.Content>
-                  <Button.Content hidden>Choose a File</Button.Content>
+        <Tab.Pane attached={false} className="center-div" style={{ display: !_.isEmpty(this.state.data) && this.state.hideForm ? 'None' : 'block' }}>
+          <Message>Upload files to convert Excel data to YAML or JSON.</Message>
+          <Form onSubmit={this.onFormSubmit}>
+            <Form.Field>
+              <label>File input & upload </label>
+              <Button as="label" htmlFor="file" type="button" animated="fade">
+                <Button.Content visible>
+                  <Icon name="file" />
+                </Button.Content>
+                <Button.Content hidden>Choose a File</Button.Content>
+              </Button>
+              <input
+                type="file"
+                id="file"
+                hidden
+                accept={SheetJSFT}
+                onChange={this.handleChange}
+              />
+              <Form.Input
+                fluid
+                label="File Chosen: "
+                placeholder="Use the above bar to browse your file system"
+                readOnly
+                value={this.state.filename}
+              />
+              <Button primary style={{ marginTop: "20px" }} type="submit" onClick={this.handleFile} disabled={this.state.disableButton}>
+                Process
                 </Button>
-                <input
-                  type="file"
-                  id="file"
-                  hidden
-                  accept={SheetJSFT}
-                  onChange={this.handleChange}
-                />
-                <Form.Input
-                  fluid
-                  label="File Chosen: "
-                  placeholder="Use the above bar to browse your file system"
-                  readOnly
-                  value={this.state.filename}
-                />
-                <Button primary style={{ marginTop: "20px" }} type="submit" onClick={this.handleFile} disabled={this.state.disableButton}>
-                  Process
-                </Button>
-              </Form.Field>
-            </Form>
-            <TransitionablePortal onClose={this.handleClose} open={this.state.showError}>
-          <Segment style={{ position: 'inherit', top: '40%', zIndex: 1000 }} color='red' inverted>
-            <Header>Unsupported file format or columns in sheet</Header>
-            <p>Make sure the columns are Modules, Parameters and Choices</p>
-            <p>Click any where to continue..!</p>
-          </Segment>
-          {/* <Button
+            </Form.Field>
+          </Form>
+          <TransitionablePortal onClose={this.handleClose} open={this.state.showError}>
+            <Segment style={{ position: 'inherit', top: '40%', zIndex: 1000 }} color='red' inverted>
+              <Header>Unsupported file format or columns in sheet</Header>
+              <p>Make sure the columns are Module, Parameters and Choices</p>
+              <p>Click any where to continue..!</p>
+            </Segment>
+            {/* <Button
                 content='Close Portal'
                 // negative
                 onClick={this.handleClose}
               /> */}
-        </TransitionablePortal>
-          </Tab.Pane>
-
-        { !_.isEmpty(this.state.data) &&
-        <Tab.Pane attached={false} className='center-div'>
-        <Grid style={{ marginTop: '40px' }}>
-          <CustomDropdown data={this.state.data} refresh={this.handleRefresh} />
-        </Grid>
+          </TransitionablePortal>
         </Tab.Pane>
+
+        {!_.isEmpty(this.state.data) &&
+          <Tab.Pane attached={false} className='center-div'>
+            <Grid>
+              <CustomDropdown data={this.state.data} refresh={this.handleRefresh} />
+            </Grid>
+          </Tab.Pane>
         }
       </>
     )
